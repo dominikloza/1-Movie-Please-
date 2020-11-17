bc817ee14bbfdc508c06ad1fce97802409b8da9e
Dominik <dominikloza110899@gmail.com>
2020-11-17 20:18:29 +0100

    Adding functional searching movie

---
+++ my-app/src/APIconfing.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://api.themoviedb.org/3";
+export const API_KEY = "bbef4d3837dce662da7cd81c33cc86ea";
--- my-app/src/components/Main_Page/SearchFilm.js
+++ my-app/src/components/Main_Page/SearchFilm.js
@@ -4,96 +4,200 @@ import Footer from "./Footer";
 import Button from "../Button";
 import {Link} from "react-router-dom";
 import fire, {db} from "../../firebase";
+import {API_KEY, API_URL} from "../../APIconfing";
+import poster from '../../resources/poster.png'
 
 const MainView = ({logged, name}) => {
 
 
-    let styleCircle = {}
-    let styleArrow = {}
-    let styleNav = {}
-    let avatarStyle = {}
+        let styleCircle = {}
+        let styleArrow = {}
+        let styleNav = {}
+        let avatarStyle = {}
 
-    const [aside, setAside] = useState(false);
+        const [aside, setAside] = useState(false);
 
-    if (aside === "clicked") {
-        styleCircle = {
-            transition: "0.5s",
-            right: "-800px"
+        if (aside === "clicked") {
+            styleCircle = {
+                transition: "0.5s",
+                right: "-800px"
+            }
+            styleArrow = {
+                transition: "0.5s",
+                right: "240px",
+                top: "70%",
+                transform: "rotate(180deg)",
+                fontSize: "55px"
+            }
+            styleNav = {
+                transition: "0.5s",
+                right: "180px"
+            }
+
+            avatarStyle = {
+                transition: "0.5s",
+                color: "#fff",
+                position: "relative",
+                zIndex: "3"
+            }
+
+        } else if (aside === true) {
+            styleCircle = {
+                transition: "0.5s",
+                right: "-1330px"
+            }
+            styleArrow = {
+                transition: "0.5s",
+                right: "40px"
+            }
+        } else if (aside === false) {
+            styleCircle = undefined;
+            styleArrow = undefined;
         }
-        styleArrow = {
-            transition: "0.5s",
-            right: "240px",
-            top: "70%",
-            transform: "rotate(180deg)",
-            fontSize: "55px"
-        }
-        styleNav = {
-            transition: "0.5s",
-            right: "180px"
-        }
-
-        avatarStyle = {
-            transition: "0.5s",
-            color: "#fff",
-            position: "relative",
-            zIndex: "3"
-        }
-
-    } else if (aside === true) {
-        styleCircle = {
-            transition: "0.5s",
-            right: "-1330px"
-        }
-        styleArrow = {
-            transition: "0.5s",
-            right: "40px"
-        }
-    } else if (aside === false) {
-        styleCircle = undefined;
-        styleArrow = undefined;
-    }
 
 
-    const logout = () => {
-        fire.auth().signOut().then(function () {
-        }).catch(function (error) {
-            console.log(error)
-        });
-    }
+        const logout = () => {
+            fire.auth().signOut().then(function () {
+            }).catch(function (error) {
+                console.log(error)
+            });
+        }
 
-    return (
-        <div className="main_page">
-            <Header style={avatarStyle} logged={logged} name={name}/>
-            <h2 className="page_text search_film">Let's choose
-                Preferences of a Movie and Click on Button below</h2>
-            <Button btnType="btn btn-primary-small" text="Find it out"/>
-            <div className="film_box">
-                <img src="" alt=""/>
-                <div className="text_box">
-                    <h2>Title</h2>
-                    <p className="film_descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid
-                        corporis, dignissimos dolor doloribus earum eos esse fugit in natus obcaecati officia omnis
-                        quaerat quas quibusdam, repellat repudiandae sit voluptate!</p>
-                    <div className="rate_scale">
-                        <span className="rate_prec">72%</span> people recommendation
-                    </div>
-                    <button>Read More</button>
+        let movieCat;
+        let catNum;
+        let listMovies;
+        let numPages;
+        let numPageMovies;
+        let movieID;
+        let lengthPage;
+        const [movie, setMovie] = useState(false);
+        let isSearched;
+        let category;
+        let page;
+        let filmNum;
+        let maxText;
+
+        const handleSearch = () => {
+            fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=bbef4d3837dce662da7cd81c33cc86ea&language=en-US`, {
+                method: 'GET'
+            })
+                .then(response => {
+                    if (response.ok === false) {
+                        throw new Error("Błąd sieci!");
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                    movieCat = data;
+                    category = Math.floor(Math.random() * movieCat.genres.length);
+                    catNum = movieCat.genres[category].id;
+
+                    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=bbef4d3837dce662da7cd81c33cc86ea&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${catNum}`, {
+                        method: 'GET'
+                    })
+                })
+                .then(response => {
+                    if (response.ok === false) {
+                        throw new Error("Błąd sieci!");
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                    listMovies = data;
+                    numPages = listMovies.total_pages;
+                    page = Math.floor(Math.random() * (numPages)) + 1;
+
+                    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=bbef4d3837dce662da7cd81c33cc86ea&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${catNum}`, {
+                        method: 'GET'
+                    })
+                })
+                .then(response => {
+                    if (response.ok === false) {
+                        throw new Error("Błąd sieci!");
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                    numPageMovies = data;
+                    lengthPage = numPageMovies.results.length;
+                    filmNum = Math.floor(Math.random() * (lengthPage));
+                    movieID = numPageMovies.results[filmNum].id;
+
+                    return fetch(` https://api.themoviedb.org/3/movie/${movieID}?api_key=bbef4d3837dce662da7cd81c33cc86ea&language=en-US`, {
+                        method: 'GET'
+                    })
+                })
+                .then(response => {
+                    if (response.ok === false) {
+                        throw new Error("Błąd sieci!");
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                    setMovie(data);
+                    isSearched = true;
+                    if(isSearched) {
+                        if ((movie.title.length > 25 || movie.genres.length > 4) || (movie.title.length > 25 && movie.genres.length > 4 )) {
+                            maxText = 100;
+                        }
+                    }
+                })
+                .catch(err => console.log(err));
+        };
+
+
+
+        return (
+            <div className="main_page">
+                <Header style={avatarStyle} logged={logged} name={name}/>
+                <h2 className="page_text search_film">Let's choose
+                    Preferences of a Movie and Click on Button below</h2>
+                <div className="btn_box">
+                    <button onClick={handleSearch} type="submit" className="btn btn-primary-small">Find it out</button>
                 </div>
+                {movie &&
+                <div className="film_box">
+                    <img src={movie.poster_path === null ? poster :`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt=""/>
+                    <div className="text_box">
+                        <h2>{movie.title}</h2>
+                        <h3><span className="original_title">({movie.original_title})</span>({movie.release_date.slice(0,4)})</h3>
+                        <div className="categories">
+                            {
+                                movie.genres.map((el) => {
+                                    return (
+                                        <span className="category">{el.name}</span>
+                                    )
+                                })
+                            }
+                        </div>
+                        <p className="film_descr">
+                            {movie.overview.length <= maxText ? movie.overview.slice(0, 300) : movie.overview.slice(0, maxText) + "..."}
+                        </p>
+                        <div className="rate_scale">
+                        </div>
+                        <button>Read More</button>
+                    </div>
+                </div>
+                }
+                <i className="fas fa-arrow-circle-left" style={styleArrow}
+                   onMouseEnter={aside === "clicked" ? undefined : () => setAside(true)}
+                   onClick={aside === "clicked" ? () => setAside(undefined) : () => setAside("clicked")}></i>
+                <nav style={styleNav}>
+                    <Link className="nav_link">Account</Link>
+                    <Link className="nav_link">History</Link>
+                    <Link className="nav_link">Friends</Link>
+                    <button className="nav_link" onClick={logout}>Sign Out</button>
+                </nav>
+                <div className="aside_nav" style={styleCircle}
+                     onMouseLeave={aside === "clicked" ? undefined : () => setAside(false)}></div>
+                <Footer/>
             </div>
-            <i className="fas fa-arrow-circle-left" style={styleArrow}
-               onMouseEnter={aside === "clicked" ? undefined : () => setAside(true)}
-               onClick={aside === "clicked" ? () => setAside(undefined) : () => setAside("clicked")}></i>
-            <nav style={styleNav}>
-                <Link className="nav_link">Account</Link>
-                <Link className="nav_link">History</Link>
-                <Link className="nav_link">Friends</Link>
-                <button className="nav_link" onClick={logout}>Sign Out</button>
-            </nav>
-            <div className="aside_nav" style={styleCircle}
-                 onMouseLeave={aside === "clicked" ? undefined : () => setAside(false)}></div>
-            <Footer/>
-        </div>
-    );
-};
+        );
+    }
+;
 
 export default MainView;
